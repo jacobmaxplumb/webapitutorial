bbe533c64567e5ce3a6d88876a5f26a032037294
jacob plumb <jacob.plumb@e-hps.com>
2018-11-16 10:45:00 -0500

    start of the project

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,41 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDto> Cities { get; set; }
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto()
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with the big park",
+                    PointsOfInterest = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Central Park",
+                            Description = "The most visited urban park in the United States"
+                        }
+                    }
+                },
+                new CityDto()
+                {
+                    Id = 2,
+                    Name = "Monroe",
+                    Description = "The small one"
+                }
+            };
+        }
+    }
+}
